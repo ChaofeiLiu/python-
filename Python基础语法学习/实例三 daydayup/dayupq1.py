684f30440f4163c31744d2f73805ec649ea2fa5f
chaofei_liu <chaofei_liuwork@163.com>
2019-10-01 12:04:54 +0800

    my first rm -- python基础语法

--- Python基础语法学习/实例三 daydayup/dayupq1.py
+++
@@ -1,4 +0,0 @@
-#dayupq1.py
-dayup =pow(1.001,365)
-daydown =pow(0.999,365)
-print("向上的力量:{:.2f},向下的力量:{:.2f}".format(dayup,daydown))
--- Python基础语法学习/文本进度条实例四/textpro2.py
+++
@@ -1,5 +0,0 @@
-#textpro2.py
-import time
-for i in range(101):
-             print("\r{:3}%".format(i),end=" ")
-             time.sleep(0.1)
